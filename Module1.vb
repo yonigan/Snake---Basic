1030e7008628da1f4feed1c6548d0a2acbfba471
yonigan <63378155+yonigan@users.noreply.github.com>
2020-05-13 20:02:59 +0100

    snake by yoni

--- Module1.vb
+++ Module1.vb
@@ -1,5 +1,5 @@
 ﻿' # Bare bones version of snake. L.Minett (2020)
-
+Imports System.IO
 Imports System.Console
 Imports System.Text
 
@@ -10,11 +10,20 @@ Module Module1
         Dim y As Short
     End Structure
 
+    Const AppleShape As Integer = &H25CF
+    Const SnakeShape As Integer = 9632
+    Const SnakeHead As Integer = 9633
     Dim SnakeTrail As New Queue(Of Coord)
     Dim GameWindow As New Coord With {.x = 80, .y = 40}
     Dim Bearing As New Coord With {.x = 1, .y = 0}
-    Dim Tail As Short = 5
+    Dim Tail As Short = 0
+    Sub PlayBackgroundSoundFile()
+        My.Computer.Audio.Play("C:\Users\yonig\OneDrive\Documents\Audacity\snake.wav",
+        AudioPlayMode.BackgroundLoop)
+    End Sub
     Sub Main()
+        Dim highscore As Integer
+        Dim score As Integer
         Dim Apple As New Coord With {.x = 5, .y = 5}
         Dim Player As New Coord With {.x = 10, .y = 10}
         Dim MovesWatch As New Stopwatch
@@ -24,11 +33,20 @@ Module Module1
         SetWindowSize(GameWindow.x, GameWindow.y)
         OutputEncoding = Encoding.UTF8 ' enables full set of Unicode characters
         CursorVisible = False
-
+        ForegroundColor = ConsoleColor.Yellow
+        SetCursorPosition(0, 0)
+        Write($"Score:{score.ToString.PadLeft(4, " ")}")
         SetCursorPosition(Apple.x, Apple.y)
         ForegroundColor = ConsoleColor.Red
-        Write("O")
-
+        Write(ChrW(AppleShape))
+        PlayBackgroundSoundFile()
+        Dim fileReader As StreamReader
+        fileReader = My.Computer.FileSystem.OpenTextFileReader("C:\Users\yonig\OneDrive\Documents\GitHub\Snake---Basic\highscore.txt")
+        highscore = fileReader.ReadLine()
+        fileReader.Close()
+        ForegroundColor = ConsoleColor.Magenta
+        SetCursorPosition(15, 0)
+        Write($"Highscore:{highscore.ToString.PadLeft(4, " ")}")
         MovesWatch.Start()
         Do
             Do
@@ -57,44 +75,76 @@ Module Module1
             Player.y += Bearing.y
 
             If Player.x <= 0 Then
-                Player.x = GameWindow.x - 1
+                'Player.x = GameWindow.x - 1
+                Finished = True
             ElseIf Player.x >= GameWindow.x Then
-                Player.x = 0
+                'Player.x = 0
+                Finished = True
             End If
 
             If Player.y <= 0 Then
-                Player.y = GameWindow.y - 1
+                'Player.y = GameWindow.y - 1
+                Finished = True
             ElseIf Player.y >= GameWindow.y Then
-                Player.y = 0
+                'Player.y = 0
+                Finished = True
             End If
+
 
             ForegroundColor = ConsoleColor.Green
             SetCursorPosition(Player.x, Player.y)
-            Write(ChrW(9608))
+            Write(ChrW(SnakeShape))
+
+
 
             While SnakeTrail.Count > Tail 'if trail queue is longer than tail size, remove from queue
                 SetCursorPosition(SnakeTrail.Peek.x, SnakeTrail.Peek.y) ' Position of earliest drawn snake trail
                 SnakeTrail.Dequeue() ' remove position from trail
                 Write(" ") ' cover snake tail
+
             End While
 
             If SnakeTrail.Contains(Player) Then
-                Tail = 5
+                Finished = True
             Else
                 SnakeTrail.Enqueue(Player)
+
             End If
 
             If Apple.x = Player.x And Apple.y = Player.y Then
                 Tail += 1
-                Apple.x = New Random().Next(0, GameWindow.x)
-                Apple.y = New Random().Next(0, GameWindow.y)
+                score += 1
+                ForegroundColor = ConsoleColor.Yellow
+                SetCursorPosition(0, 0)
+                Write($"Score:{score.ToString.PadLeft(4, " ")}")
+                Apple.x = New Random().Next(5, GameWindow.x - 5)
+                Apple.y = New Random().Next(5, GameWindow.y - 5)
                 SetCursorPosition(Apple.x, Apple.y)
                 ForegroundColor = ConsoleColor.Red
-                Write("O")
+                Write(ChrW(AppleShape))
+                interval -= 5
             End If
 
             MovesWatch.Restart()
+
         Loop Until Finished
+
+        Dim path As String = "C:\Users\yonig\OneDrive\Documents\GitHub\Snake---Basic\highscore.txt"
+        If highscore > score Then
+        Else
+            Dim fs As FileStream = File.Create(path)
+            fs.Close()
+            Dim filewrite As StreamWriter
+            filewrite = My.Computer.FileSystem.OpenTextFileWriter("C:\Users\yonig\OneDrive\Documents\GitHub\Snake---Basic\highscore.txt", True)
+            filewrite.WriteLine(score)
+            filewrite.Close()
+            highscore = score
+        End If
+        MsgBox("Game Over
+
+" & "score = " & score & "
+
+" & "highscore = " & highscore)
     End Sub
 
 End Module
